bb61235051635d5b27d6f7abef0a41ba4700e991
Matarou <slinky@honette.com>
2026-09-21 00:03:22 +0900

    0

--- run.bat
+++ run.bat
@@ -1,9 +1,27 @@
 @echo off
+setlocal
 cd /d "%~dp0"
-if exist ".venv_win\Scripts\python.exe" (
-  ".venv_win\Scripts\python.exe" -m fastrim %*
-) else if exist ".venv\Scripts\python.exe" (
-  ".venv\Scripts\python.exe" -m fastrim %*
-) else (
-  python -m fastrim %*
+
+set "PY=.venv_win\Scripts\python.exe"
+if not exist "%PY%" set "PY=.venv\Scripts\python.exe"
+
+if not exist "%PY%" (
+    echo Python virtualenv not found.
+    echo.
+    echo Run once to set up:
+    echo   python -m venv .venv_win
+    echo   .venv_win/Scripts/activate
+    echo   .venv_win/Scripts/pip install -r requirements.txt
+    echo.
+    pause
+    exit /b 1
 )
+
+"%PY%" -m fastrim %*
+set "RC=%ERRORLEVEL%"
+if not "%RC%"=="0" (
+    echo.
+    echo FasTrim exited with error code %RC%.
+    pause
+)
+endlocal
